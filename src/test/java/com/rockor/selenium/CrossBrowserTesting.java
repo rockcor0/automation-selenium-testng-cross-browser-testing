b6859dc512e523e8276e507161a0d305927131e5
Ricardo Delgado <ridel007@gmail.com>
2020-12-26 21:53:30 -0500

    testng.xml

---
+++ src/test/java/com/rockor/selenium/CrossBrowserTesting.java
@@ -0,0 +1,46 @@
+package com.rockor.selenium;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+
+public class CrossBrowserTesting {
+	
+	private WebDriver driver;
+
+	@BeforeClass
+	@Parameters({"URL", "BrowserType"})
+	public void beforeClass(String url, String browserType) {
+		
+		if(browserType.equals("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver");
+			driver = new ChromeDriver();
+		} 
+		else if(browserType.equals("Firefox")) {
+			System.setProperty("webdriver.chrome.driver", "./src/test/resources/firefoxdriver/firefoxdriver");
+			driver = new FirefoxDriver();
+		}
+		else if(browserType.equals("Internet Explorer")) {
+			System.setProperty("webdriver.ie.driver", "./src/test/resources/iedriver/iedriver");
+			driver = new InternetExplorerDriver();
+		}
+		
+	}
+
+	@Test
+	public void youtubeSearch() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
